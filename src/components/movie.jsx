c19ccc26e8230e97f451793e7525b5c568ecab3f
Mayra016 <mayrazucaro@gmail.com>
2025-08-24 11:46:12 -0300

    Fix: typing error from class to className

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,8 +1,8 @@
 
 const Movie = ({movieName, imageURL, sendData}) => {
     return(
-        <div class="movie col-6">
-            <a class="check-answer-link" onClick={() => sendData(movieName)} >
+        <div className="movie col-6">
+            <a className="check-answer-link" onClick={() => sendData(movieName)} >
                 <img src={imageURL}></img>
                 <h1>{movieName}</h1>
             </a>							
