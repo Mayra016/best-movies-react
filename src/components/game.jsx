360c3779a07f9f60b0d8712baffc4b03f8889a4a
Mayra016 <mayrazucaro@gmail.com>
2025-08-24 11:45:47 -0300

    Feature: add TheMovieDB API fetch and change background color depending on whether the answer is correct or not

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,13 +2,14 @@ import { useTranslation } from "../components/LanguageProvider";
 import Movie from "../components/movie";
 import { useState, useEffect, useRef } from "react";
 
-const Game = () => {
+const Game = ({sendData}) => {
     const {text} = useTranslation();
     const [userInput, setUserInput] = useState("");
     const [levelMovies, setLevelMovies] = useState([]);
     const apiKey = process.env.API_KEY;
     const baseImgUrl = "https://image.tmdb.org/t/p/w500";
     const gameSection = useRef(null);
+    let posibleMoves = [];
 
     const handleUserInput = (value) => {
         setUserInput(value);
@@ -22,6 +23,7 @@ const Game = () => {
                 levelMovies[0].popularity >= levelMovies[1].popularity
             ) {
                 gameSection.current?.classList.add("green");
+                sendData(true);
                 setTimeout(() => gameSection.current?.classList.remove("green"), 2000);
             } else {
                 gameSection.current?.classList.add("red");
@@ -30,19 +32,26 @@ const Game = () => {
         }
     }
 
-    useEffect(() => {
-        const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${apiKey}`;
-        fetch(url)
-            .then(res =>  {
-                console.log(res);
-                return res.json();
-            })
-            .then(data => {
-                if (data.results && data.results.length >= 2) {
-                    setLevelMovies([data.results[0], data.results[1]]);
-                }
-            })
-            .catch(err => console.error(err));
+    useEffect(() => { 
+        const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US`; 
+        console.log(apiKey); 
+        fetch(url, {
+            method: "GET",
+            headers: {
+              "Authorization": `Bearer ${apiKey}`,
+              "accept": "application/json"
+            }
+          })
+            .then(res => { 
+                console.log(res); 
+                return res.json(); 
+            }) 
+            .then(data => { 
+                console.log(data.results); 
+                posibleMoves = data.results; 
+                setLevelMovies([data.results[0], data.results[1]]); 
+            }) .
+            catch(err => console.error(err)); 
     }, []);
 
     if (levelMovies.length < 2) {
--- src/pages/play.jsx
+++ src/pages/play.jsx
@@ -5,8 +5,16 @@ import Game from '../components/game';
 const Play = () => {
     const [score, setScore] = useState(0);
 
+    const handleUserAnswer = (value) => {
+        if (value) {
+            setScore(score + 10);
+        } else {
+            setScore(0);
+        }
+    }
+
     return(
-        <><Score score={score}></Score><Game></Game></>
+        <><Score score={score}></Score><Game sedData={handleUserAnswer}></Game></>
     );
 }
 
